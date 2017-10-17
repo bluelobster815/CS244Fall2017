bdcd6f6e14bdef1aa0f7d72d0632d778bea69f23
Howard Huang <huanght@uci.edu>
2017-10-17 14:36:24 -0700

    * Changed to use HTTPClient instead of WifiClient. * Added code to get Wifi RSSI to send to server.

--- Assignment01/src/Main.cpp
+++ Assignment01/src/Main.cpp
@@ -1,18 +1,17 @@
 /**
- * Blink
+ * CS244 Assignment 1 - Sensor Node to Cloud Data Transfer
+ * 
+ * Team #7
  *
- * Turns on an LED on for one second,
- * then off for one second, repeatedly.
  */
+
 #include "Arduino.h"
 #include "ESP8266WiFi.h"
+#include "ESP8266HTTPClient.h"
 #include "Config.h"
 
 // Set LED_BUILTIN if it is not defined by Arduino framework
 // #define LED_BUILTIN 13
-
-// Library class that provides functionality for sending and recieving HTTP(S) requests and responses, respectively.
-WiFiClientSecure webClient;
 
 String getMacAddress() {
   // MAC addresses are 48 bits, i.e. 6 bytes.
@@ -29,7 +28,8 @@ void connectWiFi() {
   WiFi.disconnect();
 
   // Initialize WiFi radio and attempt to connect to specified SSID.
-  WiFi.begin(WiFiSSID); // Add password as second parameter when on a password-protected WiFi.
+  WiFi.begin(WiFiSSID, WiFiPassword);
+
   // Keep waiting/retrying till the WiFi status changes to connected.
   // Note that we will wait forever if we reach the maximum number of failed connection attemps.
   while (WiFi.status() != WL_CONNECTED) {
@@ -43,66 +43,39 @@ void connectWiFi() {
 }
 
 // Send an HTTP POST request
-void sendPostRequest(char *host, uint16_t port, char *path, const char* hostFingerprint, char* messageBody) {
+void sendPostRequest(char *host, uint16_t port, char *path, char* messageBody) {
+  
   // Attempt to establish a connection to the server.
   Serial.print("Attempting to connect to ");
   Serial.println(host);
 
-  if (webClient.connect(host, port)) {
-    // Verify website's fingerprint if we are using SSL (hostFingerprint should be left NULL/empty to indicate that we are to use regular HTTP).
-    if (hostFingerprint != NULL && hostFingerprint[0] != '\0') {
-      Serial.println("TLS request; verifying host's fingerprint...");
-      if (webClient.verify(hostFingerprint, host)) {
-        Serial.println("Fingerprint was verified; commencing request.");
-      } else {
-        Serial.println("Fingerprint does not match; abandoning request.");
-        webClient.stop();
-        return;
-      }
-    }
-
+  // Arduino library class that provides funcitonality for sending HTTP requests
+  HTTPClient http;
+  if (http.begin(host, port, path)) {
+    
     Serial.println("Successfully connected.");
     Serial.println("Sending HTTP POST request...");
     
-    Serial.println("============================");
-    // Char buffer for formatting the HTTP request.
-    // Note that in the code below, client is responsible for actually performing the HTTP request, but we also print to Serial for debugging purposes.
-    char buffer[128];
-    sprintf(buffer, "POST %s HTTP/1.1", path);
-    Serial.println(buffer);
-    webClient.println(buffer);
-    sprintf(buffer, "Host: %s", host);
-    Serial.println(buffer);
-    webClient.println(buffer);
-    sprintf(buffer, "Accept-Language: en");
-    Serial.println(buffer);
-    webClient.println(buffer);
-    sprintf(buffer, "Connection: close");
-    Serial.println(buffer);
-    webClient.println(buffer);
-    Serial.println();
-    webClient.println(); // The empty line that seperates the HTTP request's header and body sections.
-    Serial.println(messageBody);
-    webClient.println(messageBody);
-    Serial.println("============================");
+    // get and print HTTP status code
+    int httpStatusCode = http.POST(messageBody);
+    Serial.print("HTTP status code: ");
+    Serial.println(httpStatusCode);
 
-    // Now read the server's response, character by character.
-    Serial.println("Reading server's response...");
-    Serial.println("============================");
-    while (webClient.available()) {
-      char c = webClient.read();
-      Serial.print(c);
+    // if HTTP status code is not negative (meaning no errors):
+    if ( httpStatusCode > 0 ) {
+      Serial.println("=== Begin Response Payload ===");
+      String payload = http.getString();
+      Serial.println(payload);
+      Serial.println("=== End Response Payload ===");
     }
-    Serial.println();
-    Serial.println("============================");
-    
-    // Clean up client side part of connection if connection closed by server.
-    if (!webClient.connected()) {
-      webClient.stop();
-    }
+
+    // close HTTP connection
+    http.end();
+
   } else {
     Serial.println("Connection attempt failed.");
   }
+  
 }
 
 void setup()
@@ -135,22 +108,19 @@ void setup()
 
 void loop()
 {
-  sendPostRequest("varmarken.com", 443, "/tools/echo_post.php", NULL, "hello");
+  
+  // Get Wifi received signal strength indicator to send as message to server
+  int rssi = WiFi.RSSI();
+  char message[20];
+  sprintf(message, "RSSI = %d", rssi);
+
+  Serial.println();
+  Serial.print("message to send: ");
+  Serial.println(message);
+
+  sendPostRequest("172.16.0.112", 8080, "/cs244/webapi/data/upload/", message);
+  
   // Perform a request every 30 seconds.
   delay(30000);
 
-  // // turn the LED on (HIGH is the voltage level)
-  // digitalWrite(LED_BUILTIN, HIGH);
-
-  // // wait for a second
-  // delay(1000);
-
-  // // turn the LED off by making the voltage LOW
-  // digitalWrite(LED_BUILTIN, LOW);
-
-  //  // wait for a second
-  // delay(1000);
-
-  // Serial.print("MAC address: ");
-  // Serial.println(getMacAddress());
 }
