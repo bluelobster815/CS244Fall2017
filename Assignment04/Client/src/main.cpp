2ac5dc3663fcdad27d3d251b3169c798e838fd9c
Howard Huang <huanght@uci.edu>
2017-11-05 13:51:43 -0800

    Reworked the circular buffer functions to enqueue and dequeue a 3 buffers at the same time.  Added some debug code in case we run into any problems while gathering data.

--- Assignment04/Client/src/main.cpp
+++ Assignment04/Client/src/main.cpp
@@ -7,27 +7,59 @@
 #include "ESP8266HTTPClient.h"
 #include "stdio.h"
 
-#define BUF_SIZE 200
+#include "ets_sys.h"
+#include "os_type.h"
+
+extern "C" {
+#include "user_interface.h"
+}
+uint32_t freeHeapSize = system_get_free_heap_size();
+
+#define BUF_SIZE 100
+#define MESSAGE_SIZE 50
+#define SAMPLE_WINDOW_MS 1000 * 120
 
 volatile int16_t sampleBufferX[BUF_SIZE];
 volatile int16_t sampleBufferY[BUF_SIZE];
 volatile int16_t sampleBufferZ[BUF_SIZE];
 
-volatile int headIndex = 0;
-volatile int tailIndex = 0;
-volatile int bufferCount = 0;
+volatile int8_t headIndex = 0;
+volatile int8_t tailIndex = 0;
+volatile int8_t bufferCount = 0;
 
-volatile uint32_t sampleNumber = 0; //Used to make CSV output row numbers
-
-volatile bool samplesReady = false;
-
+// used for debugging
+volatile uint32_t sampleNumber = 0;
 volatile int overflowCount = 0;
+bool debugMode = false;
 
+// os_timer_t myTimer;
+LIS3DH accelerometer(SPI_MODE, 16); //Constructing with SPI interface information
+//LIS3DH accelerometer(I2C_MODE, 0x19); //Alternate constructor for I2C
 
+void enq(int16_t valueX, int16_t valueY, int16_t valueZ);
+void deq(int16_t *valueX, int16_t *valueY, int16_t *valueZ);
 
+void sensorISR(){
 
-LIS3DH myIMU(SPI_MODE, 16); //Constructing with SPI interface information
-//LIS3DH myIMU(I2C_MODE, 0x19); //Alternate constructor for I2C
+   // Serial.println("interrup fired!");
+   // uint8_t fifoUnreadSamples = ( accelerometer.fifoGetStatus() & 0x1F );
+   // uint8_t samplesToGet = fifoUnreadSamples < 20 ? fifoUnreadSamples : 20;
+   
+   while(( accelerometer.fifoGetStatus() & 0x20 ) == 0) //This checks for the 'empty' flag
+   {
+      int16_t xsample = accelerometer.readRawAccelX();
+      int16_t ysample = accelerometer.readRawAccelY();
+      int16_t zsample = accelerometer.readRawAccelZ();
+      enq(xsample,ysample,zsample );
+      sampleNumber++;
+      wdt_reset();
+      if(debugMode) {
+         Serial.print("sample count since last parse: ");
+         Serial.println(sampleNumber);
+      }
+   }
+
+}
 
 void uhoh(char* message) {
    Serial.println(message);
@@ -38,44 +70,15 @@ void uhoh(char* message) {
    }
 }
 
-// void incrementHead() {
-//    if( bufferCount <= 0 ) {
-//       uhoh();
-//    }
-//    headIndex = ( headIndex + 1 ) % BUF_SIZE;
-// }
-// void incrementTail() {
-//    if ( ( tailIndex == headIndex ) && ( bufferCount == BUF_SIZE ) ) {
-//       incrementHead();
-//    }
-//    tailIndex = ( tailIndex + 1 ) % BUF_SIZE;
-// }
-
-// void enq(int16_t buffer[], int16_t value) {
-
-//    if( bufferCount == BUF_SIZE ) {
-//       buffer[tailIndex] = value;
-//       tailIndex = ( tailIndex + 1 ) % BUF_SIZE;
-//       headIndex = ( headIndex + 1 ) % BUF_SIZE;
-//       overflowCount++;
-//    }
-//    else if ( bufferCount < BUF_SIZE ) {
-//       buffer[tailIndex] = value;
-//       tailIndex = ( tailIndex + 1 ) % BUF_SIZE;
-//       bufferCount++;
-//    }
-//    else if ( bufferCount > BUF_SIZE ) {
-//       uhoh();
-//    }
-
-// }
-
-void enq(volatile int16_t buffer[], int16_t value) {
+void enq(int16_t valueX, int16_t valueY, int16_t valueZ) {
 
    if ( bufferCount > BUF_SIZE )
       uhoh("bufferCount > BUF_SIZE");
-   
-   buffer[tailIndex] = value;
+
+   sampleBufferX[tailIndex] = valueX;
+   sampleBufferY[tailIndex] = valueY;
+   sampleBufferZ[tailIndex] = valueZ;
+
    tailIndex = ( tailIndex + 1 ) % BUF_SIZE;
 
    if( bufferCount == BUF_SIZE ) {
@@ -88,54 +91,37 @@ void enq(volatile int16_t buffer[], int16_t value) {
 
 }
 
-int16_t deq(volatile int16_t buffer[]) {
-
-   int16_t bufferValue;
+void deq(int16_t *valueX, int16_t *valueY, int16_t *valueZ) {
    
    if ( bufferCount <= 0 ) {
       uhoh("bufferCount <= 0");
    }
    else if ( bufferCount > 0 ) {
-      bufferValue = buffer[headIndex];
+      *valueX = sampleBufferX[headIndex];
+      *valueY = sampleBufferY[headIndex];
+      *valueZ = sampleBufferZ[headIndex];
+
       headIndex = ( headIndex + 1 ) % BUF_SIZE;
       bufferCount--;
    }
 
-   return bufferValue;
-}
-
-void sensorISR(){
-   samplesReady = true;
-   // Serial.println("interrup fired!");
-   while(( myIMU.fifoGetStatus() & 0x20 ) == 0) //This checks for the 'empty' flag
-   {
-      // sampleBufferX[tailIndex] = myIMU.readRawAccelX();
-      // sampleBufferY[tailIndex] = myIMU.readRawAccelY();
-      // sampleBufferZ[tailIndex] = myIMU.readRawAccelZ();
-      enq(sampleBufferX, myIMU.readRawAccelX());
-      enq(sampleBufferY, myIMU.readRawAccelY());
-      enq(sampleBufferZ, myIMU.readRawAccelZ());
-
-      sampleNumber++;
-
-   }
 }
 
 void connectWiFi() {
    Serial.print("Connecting to WiFi network");
- 
+
    // Sometimes a previous connection (that was not properly disconnected) can prevent the device from connecting to WiFi.
    // Performing a disconnect here seems to fix the issue.
    WiFi.disconnect();
- 
+
    // Initialize WiFi radio and attempt to connect to specified SSID.
    WiFi.begin(WiFiSSID, WiFiPassword);
- 
+
    // Keep waiting/retrying till the WiFi status changes to connected.
    // Note that we will wait forever if we reach the maximum number of failed connection attemps.
    while (WiFi.status() != WL_CONNECTED) {
-     delay(1000);
-     Serial.print(".");
+      delay(1000);
+      Serial.print(".");
    }
    Serial.println("");
    Serial.println("Successfully connected to WiFi network.");
@@ -145,165 +131,177 @@ void connectWiFi() {
 
 // Send an HTTP POST request
 void sendPostRequest(char *host, uint16_t port, char *path, char *messageBody) {
-   
+
    // Attempt to establish a connection to the server.
    Serial.print("Attempting to connect to ");
- 
+
    // Arduino library class that provides funcitonality for sending HTTP requests
    HTTPClient http;
    if (http.begin(host, port, path)) {
-     
-     Serial.println("Successfully connected.");
-     Serial.println("Sending HTTP POST request...");
-     
-     // get and print HTTP status code
-     int httpStatusCode = http.POST(messageBody);
-     Serial.print("HTTP status code: ");
-     Serial.println(httpStatusCode);
- 
-     // if HTTP status code is not negative (meaning no errors):
-   //   if ( httpStatusCode > 0 ) {
-   //     Serial.println("=== Begin Response Payload ===");
-   //     String payload = http.getString();
-   //     Serial.println(payload);
-   //     Serial.println("=== End Response Payload ===");
-   //   }
- 
-     // close HTTP connection
-     http.end();
- 
+
+      Serial.println("Successfully connected.");
+      Serial.println("Sending HTTP POST request...");
+
+      // get and print HTTP status code
+      int httpStatusCode = http.POST(messageBody);
+      Serial.print("HTTP status code: ");
+      Serial.println(httpStatusCode);
+
+      // if HTTP status code is not negative (meaning no errors):
+      // if ( httpStatusCode > 0 ) {
+      //    Serial.println("=== Begin Response Payload ===");
+      //    String payload = http.getString();
+      //    Serial.println(payload);
+      //    Serial.println("=== End Response Payload ===");
+      // }
+
+      // close HTTP connection
+      http.end();
+
    }
    else {
-     Serial.println("Connection attempt failed.");
+      Serial.println("Connection attempt failed.");
    }
-   
- }
+
+}
 
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
-  delay(1000); //relax...
-  Serial.println("Processor came out of reset.\n");
+   // put your setup code here, to run once:
+   Serial.begin(9600);
+   delay(1000); //relax...
+   Serial.println("===   Booting...  ===");
 
-  connectWiFi();
+   Serial.print("free heap size = ");
+   Serial.println(freeHeapSize);
+
+   connectWiFi();
+
+   accelerometer.settings.adcEnabled = 0;
+   //Note:  By also setting tempEnabled = 1, temperature data is available
+   //instead of ADC3 in.  Temperature *differences* can be read at a rate of
+   //1 degree C per unit of ADC3 data.
+   accelerometer.settings.tempEnabled = 0;
+   accelerometer.settings.accelSampleRate = 50;  //Hz.  Can be: 0,1,10,25,50,100,200,400,1600,5000 Hz
+   accelerometer.settings.accelRange = 2;      //Max G force readable.  Can be: 2, 4, 8, 16
+   accelerometer.settings.xAccelEnabled = 1;
+   accelerometer.settings.yAccelEnabled = 1;
+   accelerometer.settings.zAccelEnabled = 1;
+
+   //FIFO control settings
+   accelerometer.settings.fifoEnabled = 1;
+   accelerometer.settings.fifoThreshold = 20;  //Can be 0 to 32
+   accelerometer.settings.fifoMode = 2;
+   //fifoMode can be:
+   //  0 (Bypass mode, FIFO off)
+   //  1 (FIFO mode)
+   //  2 (Stream mode)
+   //  3 (Stream to FIFO mode)
   
-  myIMU.settings.adcEnabled = 0;
-  //Note:  By also setting tempEnabled = 1, temperature data is available
-  //instead of ADC3 in.  Temperature *differences* can be read at a rate of
-  //1 degree C per unit of ADC3 data.
-  myIMU.settings.tempEnabled = 0;
-  myIMU.settings.accelSampleRate = 50;  //Hz.  Can be: 0,1,10,25,50,100,200,400,1600,5000 Hz
-  myIMU.settings.accelRange = 2;      //Max G force readable.  Can be: 2, 4, 8, 16
-  myIMU.settings.xAccelEnabled = 1;
-  myIMU.settings.yAccelEnabled = 1;
-  myIMU.settings.zAccelEnabled = 1;
+   //Call .begin() to configure the IMU (except for the fifo)
+   accelerometer.begin();
 
-  //FIFO control settings
-  myIMU.settings.fifoEnabled = 1;
-  myIMU.settings.fifoThreshold = 20;  //Can be 0 to 32
-  myIMU.settings.fifoMode = 1;  //FIFO mode.
-  //fifoMode can be:
-  //  0 (Bypass mode, FIFO off)
-  //  1 (FIFO mode)
-  //  3 (FIFO until full)
-  //  4 (FIFO when trigger)
-  
-  //Call .begin() to configure the IMU (except for the fifo)
-  myIMU.begin();
+   Serial.print("Configuring FIFO with no error checking...");
+   accelerometer.fifoBegin(); //Configure fifo
+   Serial.print("Done!\n");
 
-  Serial.print("Configuring FIFO with no error checking...");
-  myIMU.fifoBegin(); //Configure fifo
-  Serial.print("Done!\n");
-  
-  Serial.print("Clearing out the FIFO...");
-  myIMU.fifoClear();
-  Serial.print("Done!\n");
-  myIMU.fifoStartRec(); //cause fifo to start taking data (re-applies mode bits)
+   Serial.print("Clearing out the FIFO...");
+   accelerometer.fifoClear();
+   Serial.print("Done!\n");
+   accelerometer.fifoStartRec(); //cause fifo to start taking data (re-applies mode bits)
 
-  // configure LS3IDH to generate watermark interrupt
-  uint8_t registerData = 0;
-  myIMU.readRegister(&registerData, LIS3DH_CTRL_REG3);
-//   Serial.println(registerData, BIN);
-  registerData |= 0x04;
-//   Serial.println(registerData, BIN);
-  myIMU.writeRegister(LIS3DH_CTRL_REG3, registerData);
+   // configure LS3IDH to generate watermark interrupt
+   uint8_t registerData = 0;
+   accelerometer.readRegister(&registerData, LIS3DH_CTRL_REG3);
+   // Serial.println(registerData, BIN);
+   registerData |= 0x04;
+   // Serial.println(registerData, BIN);
+   accelerometer.writeRegister(LIS3DH_CTRL_REG3, registerData);
 
-  // configure ESP8266 external interrupt
-  pinMode(4, INPUT);
-  attachInterrupt(digitalPinToInterrupt(4), sensorISR, HIGH);
+   // configure ESP8266 external interrupt
+   pinMode(4, INPUT);
+   attachInterrupt(digitalPinToInterrupt(4), sensorISR, RISING);
+
+   // os_timer_setfn(&myTimer, (os_timer_func_t *)sensorISR, NULL);
+   // os_timer_arm(&myTimer, 100, true);
+
+   Serial.println("=== Boot completed ===");
   
 }
 
 void loop()
 {
-  //float temp;  //This is to hold read data
-  //uint16_t tempUnsigned;
-  //
-//   while(( myIMU.fifoGetStatus() & 0x80 ) == 0) {wdt_reset();};  //Wait for watermark
-  
-  //Now loop until FIFO is empty.
-  //If having problems with the fifo not restarting after reading data, use the watermark
-  //bits (b5 to b0) instead.
-  //while(( myIMU.fifoGetStatus() & 0x1F ) > 2) //This checks that there is only a couple entries left
-//   if (samplesReady){
-  
-//       while(( myIMU.fifoGetStatus() & 0x20 ) == 0) //This checks for the 'empty' flag
-//       {
-//          Serial.print(sampleNumber);
-//          Serial.print(",");
-//          Serial.print(myIMU.readRawAccelX());
-//          Serial.print(",");
-//          Serial.print(myIMU.readRawAccelY());
-//          Serial.print(",");
-//          Serial.print(myIMU.readRawAccelZ());
-//          Serial.println();
-//          // myIMU.readRawAccelX();
-//          // myIMU.readRawAccelY();
-//          // myIMU.readRawAccelZ();
-//          sampleNumber++;
-//          wdt_reset();
-//       }
-//       samplesReady = false;
-//       Serial.print("sampleNumber = ");
-//       Serial.println(sampleNumber);
-//    }
 
    char entry[18] = "";
-   char message[1800] = "";
+   char message[18*MESSAGE_SIZE] = "";
+   char serverPath[] = "/cs244/webapi/data/acceleration";
 
    unsigned long loopStartTime = millis();
+   unsigned long loopWatchdog = millis();
+
+   int16_t x_sample = 0;
+   int16_t y_sample = 0;
+   int16_t z_sample = 0;
    
-   while ((millis() - loopStartTime) < 30000) {
+   while ((millis() - loopStartTime) < SAMPLE_WINDOW_MS) {
 
       wdt_reset();
 
-      if( bufferCount >= 100 ) {
+      // for debugging
+      if((millis() - loopWatchdog) > 10000)
+         debugMode = true;
+
+      // while(( accelerometer.fifoGetStatus() & 0x80 ) == 0) {wdt_reset();};  //Wait for watermark
+      // Serial.print("bufferCount before = ");
+      // Serial.println(bufferCount);
+      // Serial.println("getting samples");
+      // sensorISR();
+      // Serial.print("bufferCount after = ");
+      // Serial.println(bufferCount);
+
+      if( bufferCount >= MESSAGE_SIZE ) {
 
          Serial.println("Converting samples to CSV data....");
          Serial.print("bufferCount = ");
          Serial.println(bufferCount);
 
          detachInterrupt(4);
-         for( int i = 0; i < 100; i++ ) {
-            sprintf(entry, "%lu,%lu,%lu\n", deq(sampleBufferX), deq(sampleBufferY), deq(sampleBufferZ));
+         // os_timer_disarm(&myTimer);
+         for( int i = 0; i < MESSAGE_SIZE; i++ ) {
+            deq(&x_sample, &y_sample, &z_sample);
+            sprintf(entry, "%d,%d,%d\n", x_sample, y_sample, z_sample);
             strcat(message,entry);
          }
-         attachInterrupt(digitalPinToInterrupt(4), sensorISR, HIGH);
+         // os_timer_arm(&myTimer, 100, true);
+         attachInterrupt(digitalPinToInterrupt(4), sensorISR, RISING);
 
          sendPostRequest(hostip, 8080, serverPath, message);
 
          memset(message, 0, sizeof(message));
          message[0] = '\0';
 
+         // not sure if this is necessary
+         sensorISR();
+
+         // for debugging
+         sampleNumber = 0;
+         loopWatchdog = millis();         
+
       }
 
    }
 
+   Serial.println("loop timer expired.");
+   Serial.print("There are ");
+   Serial.print(bufferCount);
+   Serial.println(" samples left in the buffer.");
+
+   // os_timer_disarm(&myTimer);
    detachInterrupt(4);
    while ( bufferCount > 0 ) {
 
-      for( int i = 0; i < 100; i++ ) {
-         sprintf(entry, "%lu,%lu,%lu\n", deq(sampleBufferX), deq(sampleBufferY), deq(sampleBufferZ));
+      for( int i = 0; i < MESSAGE_SIZE; i++ ) {
+         deq(&x_sample, &y_sample, &z_sample);
+         sprintf(entry, "%d,%d,%d\n", x_sample, y_sample, z_sample);
          strcat(message,entry);
          if(bufferCount == 0)
             break;
@@ -314,6 +312,8 @@ void loop()
       message[0] = '\0';
 
    }
+
+   Serial.println("Done!! Wait forever...............................");
 
    while(1){
       wdt_reset();
